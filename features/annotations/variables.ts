322137f864173d017d3ca89d45d1af062b0a1179
jaydenhash02 <jaydenxian@outlook.com>
2022-11-16 03:06:10 +1100

    info on types

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -43,3 +43,23 @@ const json = '{"x": 10, "y": 20}' // json needs double quotes in string
 const coordinates: { x: number; y: number } = JSON.parse(json)
 console.log(coordinates) // {x: 10, y:20}
 
+// 2) When we declare a variable on one line 
+// and initialize it later
+let words = ["red" , "green", "blue"]
+let foundWord: boolean = false
+
+if (words.includes("green")) {
+  foundWord = true
+}
+
+console.log(foundWord)
+
+// 3) Variable whose type cannot be inferred correctly 
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false
+
+for (let num of numbers) {
+  if (num > 0) {
+    numberAboveZero = num
+  }
+}
