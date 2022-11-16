24e8aea2328ba897703943986d5829d8e7411892
jaydenhash02 <jaydenxian@outlook.com>
2022-11-17 00:46:44 +1100

    info on types

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,38 @@
-const add = (a: number, b: number) => {
-
+const add = (a: number, b: number): number => {
+  return a + b
 }
+
+const subtract = (a: number, b: number): number => {
+  return a - b
+}
+
+function divide(a: number, b: number): number {
+  return a / b
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b
+}
+
+const logger = (message: string): void => {
+  console.log(message)
+  return undefined
+}
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message)
+  }
+}
+
+const forecast = {
+  date: new Date(),
+  weather: "Sunny",
+}
+
+const logWeather = ({ date, weather }: { date: Date; weather: String }): void => {
+  console.log(date)
+  console.log(weather)
+}
+
+logWeather(forecast)
