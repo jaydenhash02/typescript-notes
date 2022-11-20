440045be6187926d7f4bee9baad518fdeed67c3d
jaydenhash02 <jaydenxian@outlook.com>
2022-11-20 17:58:42 +1100

    more notes

---
+++ maps/src/User.ts
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker"
+// .d.ts is a type definition file
+// Capital name to create and export as a class
+
+export default "red"
+
+// export it so other files can use it
+export class User {
+  // havent initialised these variables yet
+  name: string
+  location: {
+    lat: number
+    lng: number
+  }
+
+  constructor() {
+    this.name = faker.name.firstName("male")
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    }
+  }
+}
+
